ae4bc8cfb66c052829eae869b273190c503d8530
BenjaminBreuil <benjamin.breuil@jobtrek.ch>
2023-08-28 15:51:52 +0200

    Add dependency for Azure social login

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './main.css'
-import Emitter from 'tiny-emitter';
-import store from './store';
+
+
 
 
 const app = createApp(App)
